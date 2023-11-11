20e906611d52aac6b62f5083609096046c55a0ea
everettsmith928 <36865028+everettsmith928@users.noreply.github.com>
2023-11-11 10:21:03 -0700

    svg and changes

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="container-fluid">
+  <section class="container-fluid main-content">
     <!-- <section class="row navigation">
       <div class="col-6 d-flex justify-content-center">
         Here is the navigation area
@@ -19,16 +19,21 @@
         </h2>
       </div>
       <div class="col-12 splash-asset d-flex align-items-center justify-content-center">
-        <h3 class="splash-heading-text icon"><i class="icon mdi mdi-github"></i></h3>
-        <h3 class="splash-heading-text icon"><i class="icon mdi mdi-linkedin"></i></h3>
+        <a href="https://github.com/everettsmith928">
+          <h3 class="splash-heading-text icon"><i class="icon mdi mdi-github"></i></h3>
+        </a>
+        <a href="https://www.linkedin.com/in/everett-smith-811b57132/">
+          <h3 class="splash-heading-text icon"><i class="icon mdi mdi-linkedin"></i></h3>
+        </a>
         <h3 class="splash-heading-text icon"><i class="icon mdi mdi-note"></i></h3>
       </div>
     </section>
-    <section class="row banner-row">
+    <section class="row banner-row elevation-5 justify-content-center">
       <div class="col-12 d-flex justify-content-center skill-title">
         <h2 class="banner-text"><b>My Skills</b></h2>
       </div>
-      <div class="banner col-6 d-flex flex-column align-items-center">
+      <div class="banner col-3 d-flex flex-column align-items-center elevation-4">
+        <h2 class="skill-subtitle">Front End</h2>
         <h3 class="skill-text">Javascript</h3>
         <h3 class="skill-text">C#</h3>
         <h3 class="skill-text">Node JS</h3>
@@ -37,7 +42,18 @@
         <h3 class="skill-text">My SQL</h3>
         <h3 class="skill-text">Mongo</h3>
       </div>
-      <div class="banner col-6 d-flex flex-column align-items-center">
+      <div class="banner col-3 d-flex flex-column align-items-center elevation-4">
+        <h2 class="skill-subtitle">Back End</h2>
+        <h3 class="skill-text">Adobe Suite</h3>
+        <h3 class="skill-text">Photography</h3>
+        <h3 class="skill-text">Design</h3>
+        <h3 class="skill-text">Color Theory</h3>
+        <h3 class="skill-text">Mongoose</h3>
+        <h3 class="skill-text">Express</h3>
+        <h3 class="skill-text">XRPL</h3>
+      </div>
+      <div class="banner col-3 d-flex flex-column align-items-center elevation-4">
+        <h2 class="skill-subtitle">Design</h2>
         <h3 class="skill-text">Adobe Suite</h3>
         <h3 class="skill-text">Photography</h3>
         <h3 class="skill-text">Design</h3>
@@ -51,28 +67,28 @@
       <div class="col-12 d-flex justify-content-center skill-title">
         <h3 class="banner-text"><b>Projects</b></h3>
       </div>
-      <div class="col-6 d-flex justify-content-center screenshot">
+      <div class="col-5 d-flex justify-content-center screenshot">
         Project Screenshot
       </div>
-      <div class="col-6 d-flex flex-column justify-content-center project-description align-items-center g-3">
+      <div class="col-5 d-flex flex-column justify-content-center project-description align-items-center g-3">
         <h2 class="project-title">Stackify</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda sapiente officiis enim voluptatem minus
           ipsum non autem, vero nulla adipisci saepe. Corrupti nemo quos atque doloremque deleniti, distinctio optio
           exercitationem.</p>
       </div>
-      <div class="col-6 d-flex justify-content-center screenshot">
+      <div class="col-5 d-flex justify-content-center screenshot">
         Project Screenshot
       </div>
-      <div class="col-6 d-flex flex-column justify-content-center project-description align-items-center g-3">
+      <div class="col-5 d-flex flex-column justify-content-center project-description align-items-center g-3">
         <h2 class="project-title">Keepr</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda sapiente officiis enim voluptatem minus
           ipsum non autem, vero nulla adipisci saepe. Corrupti nemo quos atque doloremque deleniti, distinctio optio
           exercitationem.</p>
       </div>
-      <div class="col-6 d-flex justify-content-center screenshot">
+      <div class="col-5 d-flex justify-content-center screenshot">
         Project Screenshot
       </div>
-      <div class="col-6 d-flex flex-column justify-content-center project-description align-items-center g-3">
+      <div class="col-5 d-flex flex-column justify-content-center project-description align-items-center g-3">
         <h2 class="project-title">Tower</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda sapiente officiis enim voluptatem minus
           ipsum non autem, vero nulla adipisci saepe. Corrupti nemo quos atque doloremque deleniti, distinctio optio
@@ -91,25 +107,7 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.home {
-  display: grid;
-  height: 80vh;
-  place-content: center;
-  text-align: center;
-  user-select: none;
-
-  .home-card {
-    width: 50vw;
-
-    >img {
-      height: 200px;
-      max-width: 200px;
-      width: 100%;
-      object-fit: contain;
-      object-position: center;
-    }
-  }
-}
+.main-content {}
 
 .navigation {
   background-color: rgb(9, 9, 9);
@@ -123,17 +121,21 @@ export default {
 
 .banner {
   min-height: 20vh;
-  background-color: rgb(235, 87, 34);
+  background-color: #EB5722;
+  margin: 1rem;
+  border: 3px #efefef solid;
+  padding: 2rem;
+  border-radius: 13px;
 }
 
 .banner-row {
-  margin-left: 10vw;
-  margin-right: 10vw;
+  padding-bottom: 10vh;
 }
 
 .banner-text {
-  font-size: 3rem;
-  color: white;
+  font-size: 4rem;
+  color: #efefef;
+  font-weight: 800;
 }
 
 .skill {
@@ -146,6 +148,14 @@ export default {
 .skill-title {
   margin-top: 10vh;
   margin-bottom: 10vh;
+}
+
+.skill-subtitle {
+  color: #efefef;
+  font-size: 3rem;
+  margin: 1rem;
+  margin-bottom: 3rem;
+  font-weight: 800;
 }
 
 .skill-text {
@@ -164,6 +174,9 @@ export default {
 .splash-area {
   min-height: 100dvh;
   background-color: rgb(235, 87, 34);
+  background-image: url("../assets/img/blob-scene-haikei.svg");
+  background-position: center;
+  background-size: cover;
   border: 0px solid #ffffff;
 }
 
@@ -175,7 +188,8 @@ export default {
   font-weight: 900;
   font-size: 3rem;
   padding: 3rem;
-  color: white;
+  color: #EB5722;
+  text-shadow: #efefef 3px 3px 5px;
 }
 
 .profile-picture {
@@ -186,8 +200,7 @@ export default {
 }
 
 .project-row {
-  margin-left: 10vw;
-  margin-right: 10vw;
+  background-color: white;
 }
 
 .project-description {
