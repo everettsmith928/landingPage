ec6fe65b05acb613dd301095ff516e3c9a00aaa7
everettsmith928 <36865028+everettsmith928@users.noreply.github.com>
2023-11-15 10:06:21 -0700

    Build for deployment

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -8,7 +8,7 @@
         Here is the second nav area
       </div>
     </section> -->
-    <section class="row splash-area text-center">
+    <section class="row splash-area text-center elevation-3">
       <div class="col-12 splash-heading d-flex flex-column align-items-center justify-content-center">
         <div class="profile-picture">
           a
@@ -28,71 +28,128 @@
         <h3 class="splash-heading-text icon"><i class="icon mdi mdi-note"></i></h3>
       </div>
     </section>
-    <section class="row banner-row elevation-5 justify-content-center">
-      <div class="col-12 d-flex justify-content-center skill-title">
-        <h2 class="banner-text"><b>My Skills</b></h2>
+    <section class="row about-area justify-content-center">
+      <div class="col-12 text-center">
+        <h3 class="banner-text">About</h3>
       </div>
-      <div class="banner col-3 d-flex flex-column align-items-center elevation-4">
-        <h2 class="skill-subtitle">Front End</h2>
-        <h3 class="skill-text">Javascript</h3>
-        <h3 class="skill-text">C#</h3>
-        <h3 class="skill-text">Node JS</h3>
-        <h3 class="skill-text">Bootstrap</h3>
-        <h3 class="skill-text">Vue</h3>
-        <h3 class="skill-text">My SQL</h3>
-        <h3 class="skill-text">Mongo</h3>
+      <div class="col-5 d-flex justify-content-center">
+        Picture
+        And About area
       </div>
-      <div class="banner col-3 d-flex flex-column align-items-center elevation-4">
-        <h2 class="skill-subtitle">Back End</h2>
-        <h3 class="skill-text">Adobe Suite</h3>
-        <h3 class="skill-text">Photography</h3>
-        <h3 class="skill-text">Design</h3>
-        <h3 class="skill-text">Color Theory</h3>
-        <h3 class="skill-text">Mongoose</h3>
-        <h3 class="skill-text">Express</h3>
-        <h3 class="skill-text">XRPL</h3>
-      </div>
-      <div class="banner col-3 d-flex flex-column align-items-center elevation-4">
-        <h2 class="skill-subtitle">Design</h2>
-        <h3 class="skill-text">Adobe Suite</h3>
-        <h3 class="skill-text">Photography</h3>
-        <h3 class="skill-text">Design</h3>
-        <h3 class="skill-text">Color Theory</h3>
-        <h3 class="skill-text">Mongoose</h3>
-        <h3 class="skill-text">Express</h3>
-        <h3 class="skill-text">XRPL</h3>
+      <div class="col-5 d-flex justify-content-center">
+        Picture
+        And About area
       </div>
     </section>
-    <section class="row project-row">
+    <section class="row banner-row elevation-5 justify-content-center">
       <div class="col-12 d-flex justify-content-center skill-title">
-        <h3 class="banner-text"><b>Projects</b></h3>
+        <h2 class="banner-text"><b>Skills</b></h2>
       </div>
-      <div class="col-5 d-flex justify-content-center screenshot">
-        Project Screenshot
+      <div class="banner col-3 d-flex flex-column elevation-4">
+        <div class="text-center">
+          <h2 class="skill-subtitle">Front End</h2>
+        </div>
+        <h3 class="skill-text"><i class="mdi mdi-circle mx-4"></i>Javascript</h3>
+        <h3 class="skill-text"><i class="mdi mdi-circle mx-4"></i>Bootstrap</h3>
+        <h3 class="skill-text"><i class="mdi mdi-circle mx-4"></i>Vue</h3>
+        <h3 class="skill-text"><i class="mdi mdi-circle mx-4"></i>HTML</h3>
+        <h3 class="skill-text"><i class="mdi mdi-circle mx-4"></i>CSS</h3>
+      </div>
+      <div class="banner col-3 d-flex flex-column elevation-4">
+        <div class="text-center">
+          <h2 class="skill-subtitle">Back End</h2>
+        </div>
+        <h3 class="skill-text"><i class="mdi mdi-circle mx-4"></i>Node JS</h3>
+        <h3 class="skill-text"><i class="mdi mdi-circle mx-4"></i>C#</h3>
+        <h3 class="skill-text"><i class="mdi mdi-circle mx-4"></i>Java</h3>
+        <h3 class="skill-text"><i class="mdi mdi-circle mx-4"></i>Mongoose</h3>
+        <h3 class="skill-text"><i class="mdi mdi-circle mx-4"></i>My SQL</h3>
+        <h3 class="skill-text"><i class="mdi mdi-circle mx-4"></i>Mongo</h3>
+        <h3 class="skill-text"><i class="mdi mdi-circle mx-4"></i>Express</h3>
+        <h3 class="skill-text"><i class="mdi mdi-circle mx-4"></i>XRPL</h3>
+      </div>
+      <div class="banner col-3 d-flex flex-column elevation-4">
+        <div class="text-center">
+          <h2 class="skill-subtitle">Design</h2>
+        </div>
+        <h3 class="skill-text"><i class="mdi mdi-circle mx-4"></i>Adobe Suite</h3>
+        <h3 class="skill-text"><i class="mdi mdi-circle mx-4"></i>Photography</h3>
+        <h3 class="skill-text"><i class="mdi mdi-circle mx-4"></i>Color Theory</h3>
+        <h3 class="skill-text"><i class="mdi mdi-circle mx-4"></i>Drawing</h3>
+        <h3 class="skill-text"><i class="mdi mdi-circle mx-4"></i>Graphic Design</h3>
+        <h3 class="skill-text"></h3>
+      </div>
+    </section>
+    <section class="row project-row justify-content-center">
+      <div class="col-12 d-flex justify-content-center skill-title">
+        <h3 class="splash-heading-text"><b>Projects</b></h3>
+      </div>
+      <div class="col-5 d-flex justify-content-center align-items-center screenshot">
+        <img src="../assets/img/blob-scene-haikei.svg" class="img-fluid project-image elevation-3">
       </div>
       <div class="col-5 d-flex flex-column justify-content-center project-description align-items-center g-3">
         <h2 class="project-title">Stackify</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda sapiente officiis enim voluptatem minus
-          ipsum non autem, vero nulla adipisci saepe. Corrupti nemo quos atque doloremque deleniti, distinctio optio
-          exercitationem.</p>
-      </div>
-      <div class="col-5 d-flex justify-content-center screenshot">
-        Project Screenshot
+        <p class="project-text">Stackify is a Spotify Premium user extension that allows DJs and event planners to
+          creatively organize event
+          playlists, speeches, and write important notes while hosting an event. Built by a team of 4 developers at
+          CodeWorks we see Stackify as the capstone of our development journey at bootcamp. Utilizing the Spotify API,
+          agile development methods, and creative technical solutions. Allowing us to access rich information about songs,
+          and come up with creative ways for the user to manage and edit
+          playlists, and create a robust full-stack application within 2 weeks.</p>
+        <h2 class="project-title">Node JS Vue Mongo DB Spotify API Bootstrap</h2>
+        <h2 class="project-title"></h2>
       </div>
       <div class="col-5 d-flex flex-column justify-content-center project-description align-items-center g-3">
         <h2 class="project-title">Keepr</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda sapiente officiis enim voluptatem minus
+        <p class="project-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda sapiente officiis enim
+          voluptatem minus
           ipsum non autem, vero nulla adipisci saepe. Corrupti nemo quos atque doloremque deleniti, distinctio optio
           exercitationem.</p>
       </div>
-      <div class="col-5 d-flex justify-content-center screenshot">
-        Project Screenshot
+      <div class="col-5 d-flex justify-content-center align-items-center screenshot">
+        <img src="../assets/img/blob-scene-haikei.svg" class="img-fluid project-image elevation-3">
+      </div>
+      <div class="col-5 d-flex justify-content-center align-items-center screenshot">
+        <img src="../assets/img/blob-scene-haikei.svg" class="img-fluid project-image elevation-3">
       </div>
       <div class="col-5 d-flex flex-column justify-content-center project-description align-items-center g-3">
         <h2 class="project-title">Tower</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda sapiente officiis enim voluptatem minus
+        <p class="project-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda sapiente officiis enim
+          voluptatem minus
           ipsum non autem, vero nulla adipisci saepe. Corrupti nemo quos atque doloremque deleniti, distinctio optio
           exercitationem.</p>
+      </div>
+    </section>
+    <section class="row contact-area justify-content-center">
+      <div class="col-12 d-flex justify-content-center align-items-center">
+        <h2 class="banner-text"><b>Contact</b></h2>
+      </div>
+      <div class="col-6">
+        <form class="row contact-form elevation-3">
+          <div class="col-6 d-flex flex-column">
+            <label>
+              <h3><b>Name</b></h3>
+            </label>
+            <input type="text" class="my-3">
+          </div>
+          <div class="col-6 d-flex flex-column">
+            <label>
+              <h3><b>Email Address</b></h3>
+            </label>
+            <input type="text" class="my-3">
+          </div>
+          <div class="col-12 d-flex flex-column">
+            <label>
+              <h3><b>Title</b></h3>
+            </label>
+            <input type="text" class="my-3">
+          </div>
+          <label>
+            <h3><b>Message</b></h3>
+          </label>
+          <textarea rows="15" class="my-3"></textarea>
+          <button class="form-button">Submit</button>
+        </form>
       </div>
     </section>
   </section>
@@ -107,7 +164,15 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.main-content {}
+input {
+  height: 40px;
+  border: 2px solid #EB5722;
+  border-radius: 8px;
+}
+
+textarea {
+  border-radius: 8px;
+}
 
 .navigation {
   background-color: rgb(9, 9, 9);
@@ -119,17 +184,28 @@ export default {
   width: 1px;
 }
 
+.about-area {
+  background-color: #efefef;
+  height: 100vh;
+  padding-top: 10vh;
+
+  .banner-text {
+    color: #EB5722;
+  }
+}
+
 .banner {
-  min-height: 20vh;
-  background-color: #EB5722;
+  background-color: #efefef;
   margin: 1rem;
   border: 3px #efefef solid;
   padding: 2rem;
   border-radius: 13px;
+  transition: .3s;
 }
 
 .banner-row {
   padding-bottom: 10vh;
+  background-color: #EB5722;
 }
 
 .banner-text {
@@ -151,7 +227,7 @@ export default {
 }
 
 .skill-subtitle {
-  color: #efefef;
+  color: #EB5722;
   font-size: 3rem;
   margin: 1rem;
   margin-bottom: 3rem;
@@ -159,7 +235,7 @@ export default {
 }
 
 .skill-text {
-  color: white;
+  color: #EB5722;
 }
 
 .icon {
@@ -177,16 +253,21 @@ export default {
   background-image: url("../assets/img/blob-scene-haikei.svg");
   background-position: center;
   background-size: cover;
-  border: 0px solid #ffffff;
+  border: 20px solid #EB5722;
+}
+
+.splash-asset {
+  max-height: 5vh;
 }
 
 .splash-heading {
+  min-height: 80vh;
   color: white;
 }
 
 .splash-heading-text {
   font-weight: 900;
-  font-size: 3rem;
+  font-size: 4rem;
   padding: 3rem;
   color: #EB5722;
   text-shadow: #efefef 3px 3px 5px;
@@ -200,18 +281,62 @@ export default {
 }
 
 .project-row {
-  background-color: white;
+  background-color: #efefef;
+  padding-bottom: 20vh;
 }
 
 .project-description {
+  border-bottom: 5px solid #EB5722;
   min-height: 20vh;
-  border: 1px solid white;
-  border-radius: 8px;
+  margin-bottom: 10vh;
+}
+
+.project-text {
+  margin: 3rem;
+  font-size: 18px;
+}
+
+.screenshot {
+  min-height: 20vh;
+  margin-top: 10vh;
+  margin-bottom: 10vh;
+}
+
+.project-image {
+  border-radius: 13px;
 }
 
 .project-title {
+  font-weight: 900;
   font-size: 2rem;
   padding: 1rem;
-  color: white;
+  color: #EB5722;
+}
+
+.contact-area {
+  height: 100dvh;
+  background-image: url("../assets/img/contact-waves.svg");
+  background-size: cover;
+  background-position: center;
+
+  .banner-text {
+    color: #EB5722;
+  }
+}
+
+.contact-form {
+  background-color: #efefef;
+  border: 10px solid white;
+  padding: 1rem;
+  border-radius: 13px;
+  color: #EB5722;
+}
+
+.form-button {
+  background-color: #EB5722;
+  border: 0px;
+  border-radius: 8px;
+  color: #efefef;
+  font-size: 2rem;
 }
 </style>
