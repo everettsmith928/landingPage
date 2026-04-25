79ee5b2c8d1003cb54d35328807075fed1dc4456
Everett <everett.marsland.smith@gmail.com>
2026-04-25 13:40:25 -0600

    Colors CSS Added

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -3,7 +3,7 @@
     <div class="row">
       <div class="col-12 text-center py-5">
         <h1 class="">Everett Smith</h1>
-        <p>Test Reactivity</p>
+        <p></p>
         <p>Portfolio coming soon.</p>
       </div>
     </div>
