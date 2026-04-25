9fb085568e1cb2f8304ff320be8a330f3d60a415
Everett <everett.marsland.smith@gmail.com>
2026-04-25 11:59:29 -0600

    GASP Install

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -2,20 +2,16 @@
   <section class="container-fluid">
     <div class="row">
       <div class="col-12 text-center py-5">
-        <h1>Everett Smith</h1>
+        <h1 class="">Everett Smith</h1>
+        <p>Test Reactivity</p>
         <p>Portfolio coming soon.</p>
       </div>
     </div>
   </section>
 </template>
 
-<script>
-export default {
-  setup() {
-    return {}
-  }
-}
+<script setup>
+
 </script>
 
-<style scoped lang="scss">
-</style>
+<style scoped lang="scss"></style>
